668d23200fe7ce669d33ab881123d8003e2031c2
James <gaojun1986@gmail.com>
2016-06-24 14:48:45 +0800

    Rewrite the print pairs method to a more understandable one.

--- BracePair/src/BracePair.java
+++ BracePair/src/BracePair.java
@@ -1,35 +1,31 @@
 
 public class BracePair {
-	public BracePair()
-	{
-		 int cnt = 2;
-		    char[] array = new char[2*cnt];
-		    print_pare(cnt, cnt, array, 0);
+	static int pair = 3;
+
+	public BracePair() {
+		char[] array = new char[2 * pair];
+		print_pare(0, 0, array, 0);
 	}
-	
-	void print_pare(int l, int r, char str[], int cnt){
-	    if(l<0 || r<l) return;
-	    if(l==0 && r==0){
-	        for(int i=0; i<cnt; ++i){
-	            System.out.print(str[i]);
-	        }
-	        System.out.println(", ");
-	    }
-	    else{
-	        if(l > 0){
-	            str[cnt] = '(';
-	            print_pare(l-1, r, str, cnt+1);
-	        }
-	        if(r > l){
-	            str[cnt] = ')';
-	            print_pare(l, r-1, str, cnt+1);
-	        }
-	    }
+
+	void print_pare(int l, int r, char str[], int cnt) {
+		if (cnt >= 2 * pair) {
+			for (int i = 0; i < cnt; i++) {
+				System.out.print(str[i]);
+			}
+			System.out.println(", ");
+			return;
+		}
+		if (l < pair) {
+			str[cnt] = '(';
+			print_pare(l + 1, r, str, cnt + 1);
+		}
+		if (r < l) {
+			str[cnt] = ')';
+			print_pare(l, r + 1, str, cnt + 1);
+		}
 	}
-	
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		new BracePair();
 	}
 }
